7c27b2614ddeaeed3693b10684904f2d19aa2158
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-09-01 16:38:54 +0900

    fvm 추가 및 test파일 생성

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:remainder_flutter/main.dart';
+import 'package:remainder_flutter/providers/repeat_cycle_provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('findRepeatCycle in repeat_cycle_provider returns list', () async {
+    final provider = RepeatCycleProvider();
+    provider.findRepeatCycle();
+    expect(provider.repeatCycle, [
+      {'code': 'none', 'name': '안함'},
+      {'code': 'day', 'name': '매일'},
+      {'code': 'week', 'name': '매주'},
+      {'code': 'month', 'name': '매월'}
+    ]);
   });
 }
