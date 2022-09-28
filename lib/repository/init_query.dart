82f85a05cacf3722a3c08b2a26d6b8321c25643e
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-09-28 18:54:37 +0900

    컬럼명변경

--- lib/repository/init_query.dart
+++ lib/repository/init_query.dart
@@ -3,6 +3,6 @@ class DBConfig {
   static List<String> initQuery = [
     "CREATE TABLE repeat_cycle (code TEXT PRIMARY KEY, name TEXT)",
     "INSERT INTO repeat_cycle(code, name) VALUES('none', '안함'), ('day', '매일'), ('week', '매주'), ('month', '매월')",
-    "CREATE TABLE memo (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, noticeDate TEXT, repeat TEXT DEFAULT 'none' NOT NULL, notify_id INTEGER, is_remove INTEGER DEFAULT 0 NOT NULL,  FOREIGN KEY(repeat) REFERENCES repeat_cycle(code))"
+    "CREATE TABLE memo (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, noticeDate TEXT, repeat TEXT DEFAULT 'none' NOT NULL, notifyId INTEGER, isRemove INTEGER DEFAULT 0 NOT NULL,  FOREIGN KEY(repeat) REFERENCES repeat_cycle(code))"
   ];
 }
